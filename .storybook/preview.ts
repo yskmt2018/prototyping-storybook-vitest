64cbf98fd4d033de8ffa59259c3825bc73bd7637
Yusuke Muto <yusuke.muto@nssys.co.jp>
2026-01-25 03:51:28 +0900

    StorybookでVuetifyを適用できるようにする

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,25 @@
 import type { Preview } from '@storybook/vue3-vite'
 
+import { setup } from '@storybook/vue3'
+import { withVuetifyTheme } from './withVuetifyTheme.decorator'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+
+setup((app) => {
+  const vuetify = createVuetify({
+    components,
+    directives,
+  })
+  app.use(vuetify)
+})
+
+export const decorators = [withVuetifyTheme]
+
 const preview: Preview = {
   parameters: {
     controls: {
